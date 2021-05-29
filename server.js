b91250d034d64b0624816a2d8418e7c545fa1975
Baranimct <27barani.n@gmail.com>
2021-05-29 19:14:49 +0530

    Mongo connections

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app=express();
 
-app.get('/',(req,res) => res.send('API Running'));
+app.get('/',(req,res) => res.send('API Running Test'));
+
+//Calling DB connection
+connectDB();
 
 const PORT=process.env.PORT || 5000;
 
