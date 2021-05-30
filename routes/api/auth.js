0c690139ad1bc52f50dce991541a05dd687a8fd0
Baranimct <27barani.n@gmail.com>
2021-05-30 22:46:54 +0530

    Profile /me creation

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -42,6 +42,7 @@ router.post('/',
             }
 
             const isMatch = await bcrypt.compare(password, user.password);
+
             if (!isMatch) {
                 return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
             }
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
 
 //@route Get api/Profile 
-//@desc Test route
-//@access public
+//@desc get current users profile
+//@access private
 
-router.get('/', (req, res) => res.send("Profile route"));
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+
+        if (!profile) {
+            res.status(400).json({ msg: "There is no profile for this user" });
+        }
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Server Error");
+    }
+});
 
 module.exports = router;
