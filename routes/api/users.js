1226004418e16f79a39943686cad9863e545e748
Baranimct <27barani.n@gmail.com>
2021-05-30 11:00:22 +0530

    User Models

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-//@route Get api/Users 
-//@desc Test route
+//@route Post api/Users 
+//@desc Register Users
 //@access public
 
-router.get('/', (req, res) => res.send("Users route"));
+router.post('/',
+    [
+        check('Name', 'Name is required').not().isEmpty(),
+        check('Email', 'Please include a valid Email').isEmail(),
+        check('Password', 'Please enter the password more than 6 charadcters').isLength({ min: 6 })
+    ], (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        res.send("User route");
+    }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const connectDB = require('./config/db');
+
 const app = express();
 
 app.get('/', (req, res) => res.send('API Running Test'));
 
 //Calling DB connection
 connectDB();
+
+//Init Middle ware
+
+app.use(express.json({ extended: false }));
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
